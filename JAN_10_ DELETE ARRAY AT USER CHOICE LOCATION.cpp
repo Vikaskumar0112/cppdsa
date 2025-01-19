f376836866d0d06d25855e2e273005c6e922b8e1
Vikaskumar0112 <vikaskumar011296@gmail.com>
2025-01-19 23:32:16 +0530

    success

---
+++ JAN_10_ DELETE ARRAY AT USER CHOICE LOCATION.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {12,3,23,4,123,24,35,3,45,43,56,65,7,67,8,79,8};
+    int s = sizeof(arr)/sizeof(arr[0]);
+    for(int i=0; i<s-1;i++) cout<<arr[i]<<" ";
+    cout<<endl;    
+    int pos = 1;
+    while (pos != -1)  
+    {
+    cout<<"enter pos: ";
+    cin>>pos;
+    if(pos < 0)
+    {
+        break;
+    }
+   
+    for(int i = pos; i<s ;i++)
+    {
+        arr[i] = arr[i+1];
+    }
+    s--;
+     for(int i=0; i<s-1;i++) cout<<arr[i]<<" ";
+    cout<<endl;    
+    }
+   
+}
